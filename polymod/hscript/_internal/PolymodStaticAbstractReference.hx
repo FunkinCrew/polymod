a94538ddf181472d6ed0ef582707349103a5b91c
EliteMasterEric <ericmyllyoja@gmail.com>
2026-06-10 18:27:12 -0400

    Further restricting final and null/never access

--- polymod/hscript/_internal/PolymodStaticAbstractReference.hx
+++ polymod/hscript/_internal/PolymodStaticAbstractReference.hx
@@ -105,6 +105,16 @@ class PolymodStaticAbstractReference
   {
     if (this.absImpl != null)
     {
+      var finals = PolymodFinalMacro.getFinalsOf(this.absImpl);
+      if (finals.contains(fieldName)) {
+        throw 'Could not assign abstract static final field ${fieldName}';
+      }
+
+      var privates = PolymodFinalMacro.getPrivatePropertiesOf(this.absImpl);
+      if (privates.contains(fieldName)) {
+        throw 'Could not assign abstract static property ${fieldName}';
+      }
+
       if (Reflect.hasField(this.absImpl, fieldName))
       {
         Reflect.setProperty(this.absImpl, fieldName, fieldValue);
