7db4e95e2d9e3219dc538cec626439f8a8a9bcaa
Furo <furogaming9@gmail.com>
2026-08-06 22:28:32 +0200

    Add missing MemoryFileSystem functions

--- polymod/fs/MemoryFileSystem.hx
+++ polymod/fs/MemoryFileSystem.hx
@@ -5,6 +5,10 @@ import haxe.io.Path;
 import polymod.util.Util;
 import polymod.fs.PolymodFileSystem.PolymodFileSystemParams;
 
+#if lime
+import lime.app.Future;
+#end
+
 /**
  * This simple virtual file system demonstrates that anything can be used
  * as the backend filesystem for Polymod, as long as you can fulfill the
@@ -138,6 +142,62 @@ class MemoryFileSystem extends BaseFileSystem
   {
     return files.get(path);
   }
+
+  #if lime
+  /**
+   * Load the byte data for a file asynchronously.
+   *
+   * @param path The path to retrieve byte data from.
+   * @return A future which returns the file bytes.
+   */
+  override public function loadFileBytes(path:String):Future<Bytes>
+  {
+    var performWork:Void->Bytes = () -> {
+      var result = getFileBytes(path);
+      if (result == null)
+      {
+        throw 'Could not load file bytes $path';
+      }
+      return result;
+    };
+
+    return new Future(performWork, true);
+  }
+  #end
+
+  /**
+   * Get the byte data for a file from a specific mod.
+   *
+   * @param path The path to retrieve byte data from, relative to the asset root.
+   * @param modId A specific mod ID to retrieve an asset from.
+   * @return The file bytes, or `null` if it couldn't be fetched.
+   */
+  override public function getFileBytesByModId(path:String, modId:String):Null<Bytes>
+  {
+    var modDir:Null<String> = scanModDirectoriesForId(modId);
+    if (modDir == null) return null;
+    var relativeDir = Util.pathJoin(modRoot, modDir);
+
+    return getFileBytes(Util.pathJoin(relativeDir, path));
+  }
+
+  #if lime
+  /**
+   * Load the byte data for a file from a specific mod, asynchronously.
+   *
+   * @param path The path to retrieve byte data from, relative to the asset root.
+   * @param modId A specific mod ID to retrieve an asset from.
+   * @return A future which returns the file bytes.
+   */
+  override public function loadFileBytesByModId(path:String, modId:String):Future<Bytes>
+  {
+    var modDir:Null<String> = scanModDirectoriesForId(modId);
+    if (modDir == null) return null;
+    var relativeDir = Util.pathJoin(modRoot, modDir);
+
+    return loadFileBytes(Util.pathJoin(relativeDir, path));
+  }
+  #end
 
   /**
    * Returns a list of files contained within the provided directory path.
